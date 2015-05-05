73515b9122475cb5ee99c1083cfa9f82b1ce7dc2
Alexandre Rademaker <arademaker@gmail.com>
2015-05-05 18:07:32 -0300

    applying big for all clauses of sodoku.

--- sudoku.lisp
+++ sudoku.lisp
@@ -13,51 +13,40 @@
 
 
 (defun clauses-1 ()
-  "There is at least one number in each entry"
-  (big (and x 1 9) (big (and y 1 9) (big (or z 1 9) (atomic x z y)))))
+  "There is at least one number in each entry."
+  (big (and x 1 9)
+    (big (and y 1 9)
+      (big (or z 1 9)
+	(atomic x z y)))))
 
 
 (defun clauses-2 ()
-  "Each number appears at most once in each row"
-  (cons 'and* 
-	(loop for y from 1 to 9
-	      collect
-	      (cons 'and*
-		    (loop for z from 1 to 9
-			  collect 
-			  (cons 'and*
-				(loop for x from 1 to 8
-				      collect 
-				      (cons 'and*
-					    (loop for i from (1+ x) to 9
-						  collect
-						  `(or (not ,(atomic x y z))
-						       (not ,(atomic i y z))))))))))))
+  "Each number appears at most once in each row."
+  (big (and y 1 9)
+    (big (and z 1 9)
+      (big (and x 1 8)
+	(big (and i (1+ x) 9)
+	  `(or (not ,(atomic x y z))
+	       (not ,(atomic i y z))))))))
+
 
 (defun clauses-3 ()
-  "Each number appears at most once in each column"
-  (cons 'and*
-	(loop for y from 1 to 9
-	      collect
-	      (cons 'and*
-		    (loop for z from 1 to 9
-			  collect 
-			  (cons 'and*
-				(loop for x from 1 to 8
-				      collect 
-				      (cons 'and* 
-					    (loop for i from (1+ x) to 9
-						  collect
-						  `(or (not ,(atomic x y z))
-						       (not ,(atomic x i z))))))))))))
+  "Each number appears at most once in each column."
+  (big (and x 1 9)
+    (big (and z 1 9)
+      (big (and y 1 8)
+	(big (and i (1+ y) 9)
+	  `(or (not ,(atomic x y z))
+	       (not ,(atomic x i z))))))))
 
 
 (defun clauses-4 ()
-  "Each number appears at most once in each 3x3 sub-grid"
+  "Each number appears at most once in each 3x3 sub-grid."
   "...")
 
+(defun tabuleiro ()
+  "...")
 
-;; testando
-;; (prove* `(and* ,(clauses-3) ,(clauses-2) ,(clauses-1)))
-
-
+(defun teste-sudoku ()
+  "..."
+  (prove* `(and* ,(tabuleiro ,(clauses-3) ,(clauses-2) ,(clauses-1)))))
