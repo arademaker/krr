44cc5993fbd747b5cfabce6af088ed5516f42342
Bernardo Russo Guedes <bernardo.rgf@gmail.com>
2015-05-15 00:53:08 -0300

    Update sudoku.lisp
    alternative implementation of "at most" clauses

--- sudoku.lisp
+++ sudoku.lisp
@@ -30,6 +30,13 @@
 	       (not ,(atomic i y z))))))))
 
 
+(defun clauses-2b ()
+  "Each number appears at most once in each row"
+  (big (and y 1 9)
+    (big (and z 1 9)
+      (big (and x 1 8) `(implies ,(atomic x y z) ,`(not ,(big (or i (+ x 1) 9) (atomic i y z))))))))
+
+
 (defun clauses-3 ()
   "Each number appears at most once in each column"
   (big (and x 1 9)
@@ -40,29 +47,49 @@
 	       (not ,(atomic x i z))))))))
 
 
-(defun clauses-4-1 ()
-  (big (and z 1 9)
-    (big (and i 0 2)
-      (big (and j 0 2)
-	(big (and x 1 3)
-	  (big (and y 1 2)
-	    (big (and k (+ y 1) 3) `(or (not ,(atomic (+ x (* 3 i)) (+ y (* 3 j)) z))
-					(not ,(atomic (+ x (* 3 i)) (+ k (* 3 j)) z))))))))))
-
-(defun clauses-4-2 ()
-  (big (and z 1 9)
-    (big (and i 0 2)
-      (big (and j 0 2)
-	(big (and y 1 3)
-	  (big (and x 1 2)
-	    (big (and k (+ x 1) 3)
-	      (big (and l 1 3) `(or (not ,(atomic (+ x (* 3 i)) (+ y (* 3 j)) z))
-				    (not ,(atomic (+ k (* 3 i)) (+ l (* 3 j)) z)))))))))))
+(defun clauses-3b ()
+  "Each number appears at most once in each column"
+  (big (and x 1 9)
+    (big (and z 1 9)
+      (big (and y 1 8) `(implies ,(atomic x y z) ,`(not ,(big (or i (+ y 1) 9) (atomic x i z))))))))
 
 
 (defun clauses-4 ()
   "Each number appears at most once in each 3x3 sub-grid"
-  `(and ,(clauses-4-1) ,(clauses-4-2)))
+  `(and
+    ,(big (and z 1 9)
+      (big (and i 0 2)
+        (big (and j 0 2)
+	  (big (and x 1 3)
+	    (big (and y 1 2)
+	      (big (and k (+ y 1) 3) `(or (not ,(atomic (+ x (* 3 i)) (+ y (* 3 j)) z))
+	  				  (not ,(atomic (+ x (* 3 i)) (+ k (* 3 j)) z)))))))))
+    ,(big (and z 1 9)
+      (big (and i 0 2)
+        (big (and j 0 2)
+	  (big (and y 1 3)
+	    (big (and x 1 2)
+	      (big (and k (+ x 1) 3)
+	        (big (and l 1 3) `(or (not ,(atomic (+ x (* 3 i)) (+ y (* 3 j)) z))
+				      (not ,(atomic (+ k (* 3 i)) (+ l (* 3 j)) z))))))))))))
+
+
+(defun clauses-4b-1 ()
+  "Each number appears at most once in each 3x3 sub-grid"
+  `(and
+    ,(big (and z 1 9)
+      (big (and i 0 2)
+        (big (and j 0 2)
+	  (big (and x 1 3)
+	    (big (and y 1 2) `(implies ,(atomic (+ (* 3 i) x) (+ (* 3 j) y) z)
+	                               ,`(not ,(big (or k (+ y 1) 3) (atomic (+ (* 3 i) x) (+ (* 3 j) k) z)))))))))
+    ,(big (and z 1 9)
+      (big (and i 0 2)
+        (big (and j 0 2)
+	  (big (and y 1 3)
+	    (big (and x 1 2)
+	      (big (and l 1 3) `(implies ,(atomic (+ (* 3 i) x) (+ (* 3 j) y) z)
+	                                 ,`(not ,(big (or k (+ x 1) 3) (atomic (+ (* 3 i) k) (+ (* 3 j) k) z))))))))))))
 
 
 (defun clauses-5 ()
@@ -73,6 +100,13 @@
 	(big (and i (+ z 1) 9)
 	  `(or (not ,(atomic x y z))
 	       (not ,(atomic x y i))))))))
+
+
+(defun clauses-5b ()
+  "There is at most one number in each entry"
+  (big (and x 1 9)
+    (big (and y 1 9)
+      (big (and z 1 8) `(implies ,(atomic x y z) ,`(not ,(big (or i (+ z 1) 9) (atomic x y i))))))))
 
 
 (defun clauses-6 ()
