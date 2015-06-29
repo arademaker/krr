dc3fe754caa98632d27f4bd2486ccd2096b8fc46
Alexandre Rademaker <arademaker@gmail.com>
2015-06-29 04:18:58 -0300

    test length formula

--- tableaux-test.lisp
+++ tableaux-test.lisp
@@ -15,5 +15,7 @@
   "test the length of a formula"
   (is (= 1 (length-form 'A)))
   (is (= 3 (length-form '(implies (and A B) (and C D))))) 
-  (is (= 7 (length-form '(and (and (implies A B) (implies B A)) (and (implies B C) (implies C B)))))))
+  (is (= 7 (length-form '(and (and (implies A B) (implies B A))
+			      (and (implies B C) (implies C B))))))
+  (is (= 1 (length-form '(implies A B)))))
 
